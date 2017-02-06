52976fe385d942b4e102e07e3f18baae910a2530
Magnastraw <hintre13@gmail.com>
2017-02-07 00:59:41 +0300

    add dynamic central content

--- src/main/java/com/netcracker/smarthome/web/NavigationBean.java
+++ src/main/java/com/netcracker/smarthome/web/NavigationBean.java
@@ -5,21 +5,12 @@ import javax.faces.bean.NoneScoped;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-//
-//@ManagedBean(name = "navigationBean")
-//@SessionScoped
+
+@ManagedBean
+@RequestScoped
 public class NavigationBean {
 
-    private String pageName = "/faces/graph";
-
-    public NavigationBean() {
-    }
-
-//    public void doNav() {
-//        System.out.println("Hello");
-//        String str = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("test");
-//        this.pageName = str;
-//    }
+    private String pageName = "table";
 
     public String getPageName() {
         return pageName;
