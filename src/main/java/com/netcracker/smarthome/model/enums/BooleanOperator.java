49668739abca1a35740fb3e07792baa998b5b2f0
evarock <EvaRock13@mail.ru>
2017-03-31 13:09:59 +0400

    Fix bool operator

--- src/main/java/com/netcracker/smarthome/model/enums/BooleanOperator.java
+++ src/main/java/com/netcracker/smarthome/model/enums/BooleanOperator.java
@@ -1,48 +1,15 @@
 package com.netcracker.smarthome.model.enums;
 
-<<<<<<< HEAD
-import com.netcracker.smarthome.business.policy.conditions.Condition;
-
 public enum BooleanOperator {
     AND {
-        public boolean evaluate(Condition[] args) {
-            boolean result = true;
-            int n = args.length, i = 0;
-            while (i < n && (result = args[i].evaluate()))
-                i++;
-            return result;
-        }
-
-=======
-
-public enum BooleanOperator {
-    AND {
->>>>>>> 19b8056b9f4c5ac40ed4b5a7ec8d43997fb6af58
         public byte getPriority() {
-            return 1;
-        }
-    },
-    OR {
-<<<<<<< HEAD
-        public boolean evaluate(Condition[] args) {
-            boolean result = false;
-            int n = args.length, i = 0;
-            while (i < n && (result = args[i].evaluate()))
-                i++;
-            return result;
-        }
-
-=======
->>>>>>> 19b8056b9f4c5ac40ed4b5a7ec8d43997fb6af58
-        public byte getPriority() {
-            return 2;
-        }
-    };
-
-<<<<<<< HEAD
-    public abstract boolean evaluate(Condition[] args);
-
-=======
->>>>>>> 19b8056b9f4c5ac40ed4b5a7ec8d43997fb6af58
+                return 1;
+            }
+        },
+        OR {
+            public byte getPriority() {
+                return 2;
+            }
+        };
     public abstract byte getPriority();
 }
