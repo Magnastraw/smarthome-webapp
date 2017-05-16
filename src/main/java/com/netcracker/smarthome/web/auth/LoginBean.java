d1797eba33870bd9af93155618f2b8d4a8551f8a
Ann <noullex@gmail.com>
2017-05-17 00:39:07 +0300

    fix import in LoginBean

--- src/main/java/com/netcracker/smarthome/web/auth/LoginBean.java
+++ src/main/java/com/netcracker/smarthome/web/auth/LoginBean.java
@@ -1,7 +1,7 @@
 package com.netcracker.smarthome.web.auth;
 
 import com.netcracker.smarthome.business.auth.social.SocialServiceClient;
-import com.netcracker.smarthome.business.services.NotificationService;
+import com.netcracker.smarthome.business.notification.NotificationService;
 import com.netcracker.smarthome.dal.repositories.SmartHomeRepository;
 import com.netcracker.smarthome.web.common.ContextUtils;
 import org.slf4j.Logger;
